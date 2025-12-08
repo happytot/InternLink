38083272ccd716a2e050e5d4ed6cfe142b2404d2
happytot <acemememe@gmail.com>
2025-12-08 22:50:31 +0800

    queries in landing

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("57a5608d22e18d11")
+/******/ 		__webpack_require__.h = () => ("97abd0adfd867293")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9a1d04a3bc1409bc"; }
+/******/ 		__webpack_require__.h = function() { return "c0c7bd825077e1ba"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
@@ -830,16 +830,8 @@
 /******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
 /******/ 		var installedChunks = __webpack_require__.hmrS_jsonp = __webpack_require__.hmrS_jsonp || {
 /******/ 			"webpack": 0,
-/******/ 			"css-app_coordinator_companies_companies_css-node_modules_fontsource_inter_400_css-node_module-8e4874": 0,
-/******/ 			"css-app_components_CoordinatorSidebar_css-app_coordinator_coordinator-layout_css": 0,
-/******/ 			"css-app_index_css": 0,
-/******/ 			"css-app_intern_components_Logbook_module_css": 0,
-/******/ 			"css-app_components_InternNav_css-app_globals_css": 0,
-/******/ 			"css-app_intern_dashboard_Dashboard_css": 0,
-/******/ 			"css-app_intern_listings_Listings_css": 0,
-/******/ 			"css-app_intern_history_ApplicationHistory_css": 0,
-/******/ 			"css-app_intern_companies_companies_css": 0,
-/******/ 			"css-app_intern_profile_Profile_css": 0
+/******/ 			"css-app_LandingPage_css": 0,
+/******/ 			"css-app_globals_css-app_index_css": 0
 /******/ 		};
 /******/ 		
 /******/ 		__webpack_require__.f.j = function(chunkId, promises) {
@@ -851,7 +843,7 @@
 /******/ 					if(installedChunkData) {
 /******/ 						promises.push(installedChunkData[2]);
 /******/ 					} else {
-/******/ 						if(/^(app(\/(coordinator\/(companies\/page|layout)|intern\/(l((istings|ogbook)\/page|ayout)|(companies|dashboard|history|profile)\/page)|layout)|\-pages\-internals)|main(|\-app)|pages\/_(app|error)|\/_error|react\-refresh)$/.test(chunkId)) {
+/******/ 						if(!/^(css\-app_(LandingPage|globals_css\-app_index)_css|webpack)$/.test(chunkId)) {
 /******/ 							// setup Promise in chunk cache
 /******/ 							var promise = new Promise(function(resolve, reject) { installedChunkData = installedChunks[chunkId] = [resolve, reject]; });
 /******/ 							promises.push(installedChunkData[2] = promise);
